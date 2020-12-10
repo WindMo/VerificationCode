0c6d67b8141779e7596f4b5eae3e2ef13fdfa16f
ws <1459328339@qq.com>
2020-12-10 23:19:57 +0800

    验证码jar开发完成，附demo

---
+++ verificationcode/src/main/java/ws/common/verificationcode/support/ConstantVerifyCode.java
@@ -0,0 +1,11 @@
+package ws.common.verificationcode.support;
+
+/**
+ * @author WindShadow
+ * @date 2020/12/10
+ * @since
+ */
+public class ConstantVerifyCode {
+
+    public static final String VERIFY_CODE_SESSION_KEY = com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY;
+}
--- verificationcode/src/main/java/ws/common/verificationcode/support/web/CodeController.java
+++ verificationcode/src/main/java/ws/common/verificationcode/support/web/CodeController.java
@@ -4,7 +4,9 @@ import com.google.code.kaptcha.Constants;
 import com.google.code.kaptcha.Producer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,12 +24,13 @@ import java.io.IOException;
  */
 
 @Slf4j
+@Controller
 public class CodeController {
 
     @Autowired
     private Producer producer;
     @GetMapping("/verifyCode")
-    public void getKaptchaImage(HttpSession session, HttpServletResponse response) throws IOException {
+    public void getVerifyCodeImage(HttpSession session, HttpServletResponse response) throws IOException {
 
         // 设置响应头
         setHeaders(response);
@@ -44,11 +47,17 @@ public class CodeController {
         }
     }
 
+    /**
+     * IO异常处理方法<br>
+     * 处理请求的方法使用了{@link HttpServletResponse}作为参数，则此异常处理的返回值无法影响响应结果，
+     * 即返回值为 void的异常处理方法得搭配{@link HttpServletResponse}入参数的mapping一起用
+     * @param e 本控制层发生的IO异常
+     */
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR,reason = "验证码图片响应异常")
     @ExceptionHandler(IOException.class)
     public void exceptionHandler(IOException e) {
 
-        log.error("验证码写入客户端发生异常");
+        log.error("验证码写入客户端发生异常",e);
     }
 
     private void setHeaders(HttpServletResponse response) {
